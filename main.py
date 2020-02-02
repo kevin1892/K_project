d25577e61c8afeaa0d2eb45ed818293a581c6462
v1ct0r20 <victor.pena@utec.edu.pe>
2020-02-02 17:41:46 -0500

    first comit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas
+import sklearn
+import matplotlib
+import numpy as np
+import math
+import scipy 
+
+def getVictims(poblacion, rescatados, afectados):
+
+	return victims_now
+
+def getDamage(damage,cost, victims):
+
+	return level_danger
+
+def 
+
+#resouses_file 	1- vehiculos 
+#				2- personas o rescatistas
+#				3- carga bruta
+# 				4- tiempo de busqueda 
+#				5- ubicacion    
+#				6- ubicacion nombre
+def constructionHeuristic(file_csv, heuristic_type, enviorment, budget, victims, resouses_csv, level_danger):
+	
+	return cost, solve
